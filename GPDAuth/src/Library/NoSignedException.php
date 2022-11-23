6c2ee5df9d2e7e3c981cfd67e4190c5745145756
Jesus Abarca <jesus.abarca.g@gmail.com>
2022-11-23 17:50:59 -0600

    --no-edit

--- GPDAuth/src/Library/NoSignedException.php
+++ GPDAuth/src/Library/NoSignedException.php
@@ -10,6 +10,7 @@ class NoSignedException extends GQLException
 
     const ERROR_ID = 'AUTH_NO_SIGNED';
     const CODE = '401';
+    
     public function __construct($message = 'No signed', $errorId = NoSignedException::ERROR_ID, $httpcode = NoSignedException::CODE, $category = 'businessLogic', $previous = null)
     {
         parent::__construct($message, $httpcode, $previous);

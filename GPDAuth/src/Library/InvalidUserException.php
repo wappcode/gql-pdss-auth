a564cb2aef64a386bb9e314dbd08d002a97c48d2
Jesus Abarca <jesus.abarca.g@gmail.com>
2023-04-28 10:08:38 -0600

    Corrección excepcion InvalidUserException

--- GPDAuth/src/Library/InvalidUserException.php
+++ GPDAuth/src/Library/InvalidUserException.php
@@ -2,6 +2,7 @@
 
 namespace GPDAuth\Library;
 
+use Throwable;
 use GPDCore\Library\GQLException;
 
 class InvalidUserException extends GQLException
@@ -9,10 +10,10 @@ class InvalidUserException extends GQLException
 
 
     const ERROR_ID = 'AUTH_INVALID_USER';
-    const CODE = '400';
-    public function __construct($message = 'No signed', $errorId = InvalidUserException::ERROR_ID, $httpcode = InvalidUserException::CODE, $category = 'businessLogic', $previous = null)
+    const CODE = 400;
+    public function __construct(string $message = 'No signed', string $errorId = InvalidUserException::ERROR_ID, int $httpcode = InvalidUserException::CODE, $category = 'businessLogic', ?Throwable $previous = null)
     {
-        parent::__construct($message, $httpcode, $previous);
+        parent::__construct($message, $errorId, $httpcode, $category, $previous);
         $this->category = $category;
         $this->errorId = $errorId;
         $this->httpcode = $httpcode;

5f960bebe30f173ac88244e39a6443ed735ed1aa
Jesus Abarca <jesus.abarca.g@gmail.com>
2023-08-16 12:02:50 -0600

    Se agregó comentario para la configuración del tiempo en que expira el token

--- GPDAuth/config/module.config.php
+++ GPDAuth/config/module.config.php
@@ -5,6 +5,6 @@ use GPDAuth\Library\AuthConfig;
 return [
     AuthConfig::JWT_SECURE_KEY => "",
     AuthConfig::AUTH_SESSION_KEY => "gpd_auth_session_key", // key o indice asociativo del array $_SESSION para authentificación
-    AuthConfig::JWT_EXPIRATION_TIME_KEY => 1200,
+    AuthConfig::JWT_EXPIRATION_TIME_KEY => 1200, // Tiempo en segundos
     AuthConfig::JWT_ALGORITHM_KEY => 'HS256',
 ];

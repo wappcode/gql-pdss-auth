49b65face2fe7e36d9482351e04c0a6cbd3e3dfd
JJesus Abarca González <j.abarca@qualem.com>
2026-01-26 12:34:19 -0600

    Corrección Imports de JWTTrustIssuerRepositoryInterface

--- GPDAuthJWT/src/Models/JWTTrustIssuerRepositoryInterface.php
+++ GPDAuthJWT/src/Models/JWTTrustIssuerRepositoryInterface.php
@@ -3,13 +3,12 @@
 namespace GPDAuthJWT\Models;
 
 use GPDAuthJWT\Entities\TrustedIssuer;
-use GPDAuthJWT\Entities\TrustedIssuers;
 
 interface JWTTrustIssuerRepositoryInterface
 {
-    public function findIssuer(string $issuer): ?TrustedIssuers;
+    public function findIssuer(string $issuer): ?TrustedIssuer;
 
     public function getJWKFromIssuer(TrustedIssuer $issuer, string $keyId): ?array;
 
-    public function validateAudience(TrustedIssuer $issuer, string $audience): bool;
+    public function isValidAudience(TrustedIssuer $issuer, string $audience): bool;
 }
